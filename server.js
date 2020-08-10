140195b544f56ca20906d8e48223304d1e8df345
JoeL <jlawton1203@gmail.com>
2020-08-10 13:51:27 -0500

    added sql calls & routes to get all candidates, get specific candidate, and delete candidate

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,13 +8,113 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-//put new routes here
+//connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database.');
+});
+
+//get all candidates and return as JSON
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get single candidate and return as JSON
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//delete a single candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({error: res.message});
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+
+//database queries
+/*
+db.all(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+*/
+//get a single candidate
+/*
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+*/
+/*
+//delete a candidate
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this, this.changes);
+});
+*/
+/*
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+//ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
+*/
+
 
 //default response for any other requests(Not Found) Catch-all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+//start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
+    
