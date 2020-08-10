4d2e152c11429127289b04274f59decce6368560
JoeL <jlawton1203@gmail.com>
2020-08-10 14:08:47 -0500

    added post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -50,7 +51,7 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
-//delete a single candidate
+//delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
@@ -67,44 +68,31 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-
-//database queries
-/*
-db.all(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
-*/
-//get a single candidate
-/*
-db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
-});
-*/
-/*
-//delete a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this, this.changes);
-});
-*/
-/*
 //create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-//ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
+app.post('/api/candidate', ({body}, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if(errors) {
+        res.status(400).json({error: errors});
+        return;
     }
-    console.log(result, this.lastID);
-});
-*/
 
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                 VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    //ES5 function, not arrow function to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 //default response for any other requests(Not Found) Catch-all
 app.use((req, res) => {
